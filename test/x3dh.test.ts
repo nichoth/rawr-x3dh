a056b229043cbfd92e896e5b6cec1614744142af
nichoth <nichoth@nichoth.com>
2024-10-29 11:36:43 -0700

    wip

--- test/x3dh.test.ts
+++ test/x3dh.test.ts
@@ -1,113 +1,119 @@
 import { test } from '@bicycle-codes/tapzero'
-import { X3DH } from '../index.js'
+import {
+    type InitSenderInfo,
+    type PreKeyPair,
+    X3DH
+} from '../index.js'
 import type { Ed25519PublicKey, Ed25519SecretKey } from 'sodium-plus'
 import { SodiumPlus } from 'sodium-plus'
 
-test('X3DH -- generateOneTimeKeys', async (t) => {
-    const sodium = await SodiumPlus.auto()
-    const keypair = await sodium.crypto_sign_keypair()
-    const sk: Ed25519SecretKey = await sodium.crypto_sign_secretkey(keypair)
-    const x3dh = new X3DH()
-    const response = await x3dh.generateOneTimeKeys(sk, 4)
-    t.equal(response.bundle.length, 4, 'should return corrent bundle size')
-    t.equal(response.signature.length, 128, 'signature is correct length')
-})
+test('__X3DH E2E tests__')
 
-test('X3DH -- Handshake', async t => {
-    const sodium = await SodiumPlus.auto()
+const fox_x3dh = new X3DH()
+const wolf_x3dh = new X3DH()
+let fox_sk:Ed25519SecretKey
+let wolf_pre:PreKeyPair
+let fox_pk:Ed25519PublicKey
+let wolf_pk:Ed25519PublicKey
+let wolf_sk:Ed25519SecretKey
+let sodium:Awaited<ReturnType<typeof SodiumPlus.auto>>
+// let fox_pre:PreKeyPair
+test('X3DH Handshake with One-Time Keys', async t => {
+    sodium = await SodiumPlus.auto()
 
     // 1. Generate identity keys
     const fox_keypair = await sodium.crypto_sign_keypair()
-    const fox_sk:Ed25519SecretKey = await sodium.crypto_sign_secretkey(fox_keypair)
-    const fox_pk:Ed25519PublicKey = await sodium.crypto_sign_publickey(fox_keypair)
+    fox_sk = await sodium.crypto_sign_secretkey(fox_keypair)
+    fox_pk = await sodium.crypto_sign_publickey(fox_keypair)
     const wolf_keypair = await sodium.crypto_sign_keypair()
-    const wolf_sk:Ed25519SecretKey = await sodium.crypto_sign_secretkey(wolf_keypair)
-    const wolf_pk:Ed25519PublicKey = await sodium.crypto_sign_publickey(wolf_keypair)
+    wolf_sk = await sodium.crypto_sign_secretkey(wolf_keypair)
+    wolf_pk = await sodium.crypto_sign_publickey(wolf_keypair)
 
-    // 2. Instantiate object with same config (defaults)
-    const fox_x3dh = new X3DH()
-    const wolf_x3dh = new X3DH()
+    // 4. Generate some one-time keys
+    const fox_bundle = await fox_x3dh.generateOneTimeKeys(fox_sk, 3)
+    const wolf_bundle = await wolf_x3dh.generateOneTimeKeys(wolf_sk, 3)
+
+    // 5. Do an initial handshake from fox->wolf
+    const message = 'hewwo UwU'
+    const sent = await fox_x3dh.initSend('wolf', wolfResponse, message)
+
+    // 6. Pass the handshake to wolf->fox
+    const [sender, recv] = await wolf_x3dh.initRecv(sent)
+    t.equal(sender, 'fox', 'should have "fox" as sender')
+    t.equal(recv.toString(), message, 'should decrypt the message')
+})
+
+test('2. Instantiate object with same config (defaults)', async t => {
     await fox_x3dh.identityKeyManager.setIdentityKeypair(fox_sk, fox_pk)
     await fox_x3dh.setIdentityString('fox')
-    t.equal(await fox_x3dh.identityKeyManager.getMyIdentityString(), 'fox',
-        'should return the right ID string')
-
     await wolf_x3dh.identityKeyManager.setIdentityKeypair(wolf_sk, wolf_pk)
     await wolf_x3dh.setIdentityString('wolf')
 
-    t.equal(await wolf_x3dh.identityKeyManager.getMyIdentityString(), 'wolf',
-        'wolf should return the right ID string')
+    t.equal('fox', await fox_x3dh.identityKeyManager.getMyIdentityString(),
+        'should return the right identity string')
+
+    t.equal('wolf', await wolf_x3dh.identityKeyManager.getMyIdentityString(),
+        "should return wolf's identity string")
 })
 
-// describe('X3DH End-to-End Tests', async () => {
-//     it('X3DH Handshake with One-Time Keys', async() => {
-//         const sodium = await SodiumPlus.auto();
+test('3. Generate a pre-key for each.', async t => {
+    // const fox_pre = await fox_x3dh.identityKeyManager.getPreKeypair()
+    wolf_pre = await wolf_x3dh.identityKeyManager.getPreKeypair()
+    t.ok(wolf_pre, 'should return a pre keypair')
+})
 
-//         // 1. Generate identity keys
-//         const fox_keypair = await sodium.crypto_sign_keypair();
-//         const fox_sk: Ed25519SecretKey = await sodium.crypto_sign_secretkey(fox_keypair);
-//         const fox_pk: Ed25519PublicKey = await sodium.crypto_sign_publickey(fox_keypair);
-//         const wolf_keypair = await sodium.crypto_sign_keypair();
-//         const wolf_sk: Ed25519SecretKey = await sodium.crypto_sign_secretkey(wolf_keypair);
-//         const wolf_pk: Ed25519PublicKey = await sodium.crypto_sign_publickey(wolf_keypair);
+test('4. Generate some one-time keys', async t => {
+    const fox_bundle = await fox_x3dh.generateOneTimeKeys(fox_sk, 3)
+    const wolf_bundle = await wolf_x3dh.generateOneTimeKeys(wolf_sk, 3)
 
-//         // 2. Instantiate object with same config (defaults)
-//         const fox_x3dh = new X3DH();
-//         const wolf_x3dh = new X3DH();
-//         await fox_x3dh.identityKeyManager.setIdentityKeypair(fox_sk, fox_pk);
-//         await fox_x3dh.setIdentityString('fox');
-//         expect('fox').to.be.equal(await fox_x3dh.identityKeyManager.getMyIdentityString());
-//         await wolf_x3dh.identityKeyManager.setIdentityKeypair(wolf_sk, wolf_pk);
-//         await wolf_x3dh.setIdentityString('wolf');
-//         expect('wolf').to.be.equal(await wolf_x3dh.identityKeyManager.getMyIdentityString());
+    t.ok(fox_bundle)
+    t.ok(wolf_bundle)
+})
 
-//         // 3. Generate a pre-key for each.
-//         const fox_pre = await fox_x3dh.identityKeyManager.getPreKeypair();
-//         const wolf_pre = await wolf_x3dh.identityKeyManager.getPreKeypair();
+let sent:InitSenderInfo
+let message:string
+test('5. Do an initial handshake from fox->wolf', async t => {
+    message = 'hewwo UwU'
+    sent = await fox_x3dh.initSend('wolf', wolfResponse, message)
+    t.ok(sent)
+})
 
-//         // 4. Generate some one-time keys
-//         const fox_bundle = await fox_x3dh.generateOneTimeKeys(fox_sk, 3);
-//         const wolf_bundle = await wolf_x3dh.generateOneTimeKeys(wolf_sk, 3);
+test('6. Pass the handshake to wolf->fox', async t => {
+    const [sender, recv] = await wolf_x3dh.initRecv(sent)
+    t.equal(sender, 'fox', 'sender should be "fox"')
+    t.equal(recv.toString(), message, 'should decrypt the message')
+})
 
-//         const wolfResponse = async() => {
-//             const sig = await signBundle(wolf_sk, [wolf_pre.preKeyPublic]);
-//             return {
-//                 IdentityKey: await sodium.sodium_bin2hex(wolf_pk.getBuffer()),
-//                 SignedPreKey: {
-//                     Signature: await sodium.sodium_bin2hex(sig),
-//                     PreKey: await sodium.sodium_bin2hex(wolf_pre.preKeyPublic.getBuffer())
-//                 },
-//                 OneTimeKey: wolf_bundle.bundle[0]
-//             };
-//         };
+test('send and recieve a few more', async t => {
+    // Send and receive a few more:
+    for (let i = 0; i < 20; i++) {
+        try {
+            const plain = `OwO what's this? ${i}`
 
-//         // 5. Do an initial handshake from fox->wolf
-//         const message = "hewwo UwU";
-//         const sent = await fox_x3dh.initSend('wolf', wolfResponse, message);
+            if ((i % 3) === 0) {
+                const cipher = await wolf_x3dh.encryptNext('fox', plain)
+                const decrypt = await fox_x3dh.decryptNext('wolf', cipher)
+                t.equal(decrypt.toString(), plain, 'should decrypt the text')
+            } else {
+                const cipher = await fox_x3dh.encryptNext('wolf', plain)
+                const decrypt = await wolf_x3dh.decryptNext('fox', cipher)
+                t.equal(decrypt.toString(), plain, `round ${i + 1} is ok`)
+            }
+        } catch (e) {
+            console.log('Failed at i = ' + i)
+            throw e
+        }
+    }
+})
 
-//         // 6. Pass the handshake to wolf->fox
-//         const [sender, recv] = await wolf_x3dh.initRecv(sent);
-//         expect(sender).to.be.equal('fox');
-//         expect(recv.toString()).to.be.equal(message);
-
-//         // Send and receive a few more:
-//         for (let i = 0; i < 20; i++) {
-//             try {
-//                 const plain = `OwO what's this? ${i}`;
-//                 if ((i % 3) === 0) {
-//                     const cipher = await wolf_x3dh.encryptNext('fox', plain);
-//                     const decrypt = await fox_x3dh.decryptNext('wolf', cipher);
-//                     expect(decrypt.toString()).to.be.equal(plain, `round ${i + 1}`);
-//                 } else {
-//                     const cipher = await fox_x3dh.encryptNext('wolf', plain);
-//                     const decrypt = await wolf_x3dh.decryptNext('fox', cipher);
-//                     expect(decrypt.toString()).to.be.equal(plain, `round ${i + 1}`);
-//                 }
-//             } catch (e) {
-//                 console.log("Failed at i = " + i);
-//                 throw e;
-//             }
-//         }
-
-//     });
-// });
+async function wolfResponse () {
+    const sig = await signBundle(wolf_sk, [wolf_pre.preKeyPublic]);
+    return {
+        IdentityKey: await sodium.sodium_bin2hex(wolf_pk.getBuffer()),
+        SignedPreKey: {
+            Signature: await sodium.sodium_bin2hex(sig),
+            PreKey: await sodium.sodium_bin2hex(wolf_pre.preKeyPublic.getBuffer())
+        },
+        OneTimeKey: wolf_bundle.bundle[0]
+    }
+}
